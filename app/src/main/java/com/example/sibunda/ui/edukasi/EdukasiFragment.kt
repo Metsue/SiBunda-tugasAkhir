6f376944cc7a233421718ab024a2be63fde21c38
expransa52 <expransasaputra@gmail.com>
2026-06-02 22:56:07 +0800

    menampilkan card video dan ebook

--- app/src/main/java/com/example/sibunda/ui/edukasi/EdukasiFragment.kt
+++ app/src/main/java/com/example/sibunda/ui/edukasi/EdukasiFragment.kt
@@ -1,24 +1,30 @@
 package com.example.sibunda.ui.edukasi
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.sibunda.R
 import com.example.sibunda.databinding.FragmentEdukasiBinding
 
-class EdukasiFragment : Fragment(R.layout.fragment_edukasi) {
+class EdukasiFragment : Fragment() {
 
     private var _binding: FragmentEdukasiBinding? = null
     private val binding get() = _binding!!
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentEdukasiBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        _binding = FragmentEdukasiBinding.bind(view)
-
-        binding.btnBack.setOnClickListener {
-            requireActivity().onBackPressedDispatcher.onBackPressed()
-        }
 
         binding.btnVideoEdu.setOnClickListener {
             findNavController().navigate(R.id.action_materi_to_video)
@@ -26,6 +32,10 @@ class EdukasiFragment : Fragment(R.layout.fragment_edukasi) {
 
         binding.btnEbookEdu.setOnClickListener {
             findNavController().navigate(R.id.action_materi_to_ebook)
+        }
+
+        binding.btnBack.setOnClickListener {
+            findNavController().navigateUp()
         }
     }
 
